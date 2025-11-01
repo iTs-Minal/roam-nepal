6a18873ead7f3fe09562aa8fa66df1963f2d29da
Minal Pudasainee <pudasaineeminal@gmail.com>
2025-11-02 02:14:36 +0545

    well 2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,6 +15,7 @@ import { useState } from "react";
 
 export default function Home() {
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [isSticky, setIsSticky] = useState(false);
 
   return (
